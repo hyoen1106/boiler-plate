58cb7912370c198674596c939b0a0db898cf20ce
hyoen1106 <83644608+hyoen1106@users.noreply.github.com>
2022-02-09 16:46:06 +0900

    두번째 커밋 - 포스트맨, 노드몬 다운

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const req = require('express/lib/request')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded 를 분석해서 가져올 수 있게 함
+app.use(bodyParser.urlencoded({extended: true}));
+//pplication/json 을 분석해서 가져올 수 있게 함
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://suhyeon:abcd1234@cluster0.kniot.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
@@ -8,9 +16,25 @@ mongoose.connect('mongodb+srv://suhyeon:abcd1234@cluster0.kniot.mongodb.net/myFi
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World! ㅎㅎ 하이~')
+  res.send('Hello World! ㅎㅎ 하이~ 노드몬이당~~')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+
+app.post('/register', (req, res) => {
+
+  //회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({success : false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
